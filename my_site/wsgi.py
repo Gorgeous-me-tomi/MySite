0c2b840c6d7d5d290c6fdf616ecdf193cb182a35
Gorgeous T <tomisinerinle4@gmail.com>
2022-11-06 00:51:25 +0100

    changed what import from whitenoise

--- my_site/wsgi.py
+++ my_site/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_site.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
 
