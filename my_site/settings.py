8050e7ca0d1ad6f0782da668471e8dbb26b1fe8c
Gorgeous T <tomisinerinle4@gmail.com>
2022-11-05 23:39:25 +0100

    setting allowed host

--- my_site/settings.py
+++ my_site/settings.py
@@ -21,16 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get['SECRET_KEY','DEFAULT_KEY']
+SECRET_KEY = os.environ.get['DJANGO_SECRET_KEY','DEFAULT_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '192.168.1.161',
-    '192.168.43.79'
-]
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
