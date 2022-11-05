2ee7c8a1d73f99243302fc3b010f2dfe86290b20
Gorgeous T <tomisinerinle4@gmail.com>
2022-11-05 16:56:57 +0100

    Feat: Removed Django secret key

--- my_site/settings.py
+++ my_site/settings.py
@@ -15,13 +15,13 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# 'django-insecure-(ou&=24nq=_q5%(iigyzoa_2i1y-s3z^k*_aups3kwa3spyg&x'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ou&=24nq=_q5%(iigyzoa_2i1y-s3z^k*_aups3kwa3spyg&x'
+SECRET_KEY = os.environ.get['SECRET_KEY','DEFAULT_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
