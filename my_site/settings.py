a04e78acd608d2630246b3b96e9eea1b6b152a29
Gorgeous T <tomisinerinle4@gmail.com>
2023-03-05 10:55:17 +0100

    Changed db to db.sqlite3

--- my_site/settings.py
+++ my_site/settings.py
@@ -90,11 +90,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
 
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    # "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
